6e5db752e3fc4fe6dd72e1c7ac5aafa87d9e0818
Emre <E22536@eon.com>
2022-07-22 09:36:35 +0200

    Scroll work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Page from "./Page";
+import ScrollPage from "./scrollPage";
 
 function App() {
   return (
     <div className="App">
-      <Page />
+      <Routes>
+        <Route path="/" element={<Page />}></Route>
+        <Route path="/scroll" element={<ScrollPage />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/scrollPage/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import React, { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+
+export default function ScrollPage() {
+  return <div>SHow</div>;
+}
